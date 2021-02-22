f14ed0c9b24b84f224cfe426ec72df92f2d2a0df
pavlo-ilkanych <pasha.ilkanich@gmail.com>
2021-02-22 17:47:55 +0200

    First commit.

---
+++ src/com/company/Hw20February/Main.java
@@ -0,0 +1,43 @@
+package com.company.Hw20February;
+
+public class Main {
+    public static void main(String[] args) {
+        Warrior warrior = new Warrior(1000, new Weapon(200), "Gladiator");
+        System.out.println("Воїн: " + warrior.getName());
+        System.out.println("hp: " + warrior.getHp());
+        System.out.println("Weapon: sword. Damage: " + warrior.getDmg());
+        System.out.println();
+
+        Archer archer = new Archer(800, new Weapon(400), "Archer");
+        System.out.println("Воїн: " + archer.getName());
+        System.out.println("hp: " + archer.getHp());
+        System.out.println("Weapon: bow. Damage: " + archer.getDmg());
+        System.out.println();
+
+        Rogue rogue = new Rogue(900,new Weapon(300), "Rogue");
+        System.out.println("Воїн: " + rogue.getName());
+        System.out.println("hp: " + rogue.getHp());
+        System.out.println("Weapon: dagger. Damage: " + rogue.getDmg());
+        System.out.println();
+
+        Warrior[] warriorArr = {warrior, archer, rogue};
+
+        System.out.println("Fight !!!");
+        System.out.println();
+
+        while (warrior.getHp() >= 0 ) {
+
+            int attacking = (int)Math.floor(Math.random() * warriorArr.length);
+            int defending = (int)Math.floor(Math.random() * warriorArr.length);
+
+            if (attacking != defending){
+                warriorArr[attacking].attack(warriorArr[defending]);
+                System.out.println(warriorArr[attacking].getName() + " hit " + warriorArr[defending].getName());
+                System.out.println(warriorArr[defending].getName() + " hp: " + warriorArr[defending].getHp());
+                System.out.println();
+            }
+            else
+                continue;
+        }
+    }
+}
---
+++ src/com/company/Hw20February/Rogue.java
@@ -0,0 +1,8 @@
+package com.company.Hw20February;
+
+public class Rogue extends Warrior {
+
+    public Rogue(int hp, Weapon weapon, String name) {
+        super(hp, weapon, name);
+    }
+}
