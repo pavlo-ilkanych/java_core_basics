5c541721d9b72217e5f806cb300046983c7ec57a
pavlo-ilkanych <pasha.ilkanich@gmail.com>
2021-03-02 22:07:27 +0200

    Final commit.

---
+++ src/com/company/Hw02March/Main.java
@@ -0,0 +1,26 @@
+package com.company.Hw02March;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str;
+        str = "I like Java !!!";
+
+        System.out.println(str.charAt(14));
+        System.out.println(str.endsWith("!!!"));
+        System.out.println(str.startsWith("I like"));
+        System.out.println(str.compareTo("Java"));
+        System.out.println(str.indexOf("Java"));
+        System.out.println(str.replace("a","o"));
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.substring(7,11));
+
+        WordSize size = new WordSize();
+        size.wordSize();
+        size.wordStartingWithA();
+
+    }
+}
