4db3db3e5e8f795be829432fcdd4a53411d40a5d
pavlo-ilkanych <pasha.ilkanich@gmail.com>
2021-03-02 12:51:52 +0200

    Final commit.

--- src/com/company/HwFebruary28/Main.java
+++ src/com/company/HwFebruary28/Main.java
@@ -11,22 +11,30 @@ public class Main {
         SumOfTwoNumbers sumOfTwoNumbers = new SumOfTwoNumbers();
         Methods methods = new Methods();
 
-        methods.setA(5);
-        methods.setB(5);
+
+        try {
+
+            methods.addition(5, 5);
+            methods.division(-5, -5);
+            methods.subtraction(5, 0);
+            methods.multiplication(0, 0);
+
+        }catch (IllegalArgumentException | ArithmeticException | IllegalAccessException | MyException e){
+            e.printStackTrace();
+        }
 
 
-//
-//       try {
-//        checkNumber.CheckNumber();
-//        }catch (InputMismatchException e){
-//        System.err.println("Enter only integer!");
-//       }
-//
-//       try {
-//           sumOfTwoNumbers.SumOfTwoNumbers();
-//       }catch (InputMismatchException e){
-//           System.err.println("Enter only integer!");
-//       }
+        try {
+        checkNumber.CheckNumber();
+        }catch (InputMismatchException e){
+            System.err.println("Enter only integer!");
+        }
+
+        try {
+           sumOfTwoNumbers.SumOfTwoNumbers();
+        }catch (InputMismatchException e){
+           System.err.println("Enter only integer!");
+        }
     }
 }
 
--- src/com/company/HwFebruary28/Methods.java
+++ src/com/company/HwFebruary28/Methods.java
@@ -9,10 +9,7 @@ public class Methods {
         return a;
     }
 
-    public void setA(double a) throws MyException {
-            if (a > 0) {
-                throw new MyException("Wrong!");
-            }
+    public void setA(double a){
         this.a = a;
     }
 
@@ -24,22 +21,34 @@ public class Methods {
         this.b = b;
     }
 
-    public double division() {
+    public double division(double a, double b) throws IllegalArgumentException {
+        if (a < 0 && b < 0){
+            throw new IllegalArgumentException("Wrong!");
+        }
         double div = a / b;
         return div;
     }
 
-    public double subtraction() {
+    public double subtraction(double a, double b) throws ArithmeticException{
+        if(a == 0 && b!= 0 || a!=0 && b == 0){
+            throw new ArithmeticException("Wrong!");
+        }
         double sub = a - b;
         return sub;
     }
 
-    public double multiplication() {
+    public double multiplication(double a, double b) throws  IllegalAccessException{
+        if(a == 0 && b == 0){
+            throw new IllegalAccessException("Wrong!");
+        }
         double multi = a * b;
         return multi;
     }
 
-    public double addition() {
+    public double addition(double a, double b) throws MyException {
+        if (a > 0 && b > 0) {
+            throw new MyException("Wrong!");
+        }
         double add = a + b;
         return add;
     }
