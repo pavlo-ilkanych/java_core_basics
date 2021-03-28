2410e8187759b13f51c55cc2f24e72b500f5a3c1
pavlo-ilkanych <pasha.ilkanich@gmail.com>
2021-03-28 16:44:26 +0300

    Task 4 з доповненням.

--- src/com/company/Hw27March/Main.java
+++ src/com/company/Hw27March/Main.java
@@ -7,18 +7,21 @@ import java.util.Collection;
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        Methods methods = new Methods();
+        String path = "C:\\Users\\Twist3r\\IdeaProjects\\Homeworks\\src\\com\\company\\Hw27March\\serialize.txt";
         Employee employee = new Employee("Jack", 123456, new Salary(1000));
 
-        methods.serialize(employee, "C:\\Users\\Twist3r\\IdeaProjects\\Homeworks\\src\\com\\company\\Hw27March\\serialize.txt");
-        methods.deserialize("C:\\Users\\Twist3r\\IdeaProjects\\Homeworks\\src\\com\\company\\Hw27March\\serialize.txt");
+        Methods.serialize(employee, path);
+        Employee employeeRestored = Methods.deserialize(path);
+
+        System.out.println(employeeRestored);
 
         Collection<Employee> collection = new ArrayList<>();
 
         collection.add(employee);
         collection.add(new Employee("John", 147258, new Salary(1500)));
 
-        methods.serialize(collection, "C:\\Users\\Twist3r\\IdeaProjects\\Homeworks\\src\\com\\company\\Hw27March\\serialize.txt");
-        methods.deserialize("C:\\Users\\Twist3r\\IdeaProjects\\Homeworks\\src\\com\\company\\Hw27March\\serialize.txt");
+        Methods.serialize(collection, path);
+        Collection<Employee> collectionRestored = Methods.deserialize(path);
+        System.out.println(collectionRestored);
     }
 }
--- src/com/company/Hw27March/Methods.java
+++ src/com/company/Hw27March/Methods.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Methods implements Serializable {
 
-    public void serialize(Object obj, String path)
+    public static void serialize(Object obj, String path)
     {
         try (FileOutputStream outputStream = new FileOutputStream(path);
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream))
@@ -15,14 +15,13 @@ public class Methods implements Serializable {
         }
     }
 
-    public  <K> K deserialize(String path){
+    public static  <K> K deserialize(String path){
 
         K obj = null;
         try (FileInputStream fileInputStream = new FileInputStream(path);
         ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream))
         {
             obj = (K) objectInputStream.readObject();
-            System.out.println(obj);
         }catch (IOException | ClassNotFoundException e) {
             System.err.println(e.getMessage());
         }
