961484d6f66813c7274f6bbf6f89463e6102472d
pavlo-ilkanych <pasha.ilkanich@gmail.com>
2021-04-06 15:06:32 +0300

    Task Stream API

--- src/com/company/Hw06April/Main.java
+++ src/com/company/Hw06April/Main.java
@@ -20,36 +20,45 @@ public class Main {
         peoples.stream()
                 .filter(people -> people.getAge() >= 18 && people.getAge() < 27 && people.getSex().equalsIgnoreCase("male"))
                 .forEach(System.out::println);
+
         //Знайти середній вік серед чоловіків
         peoples.stream()
                 .filter(people -> people.getSex().equalsIgnoreCase("male"))
                 .mapToInt(People::getAge).average()
                 .getAsDouble();
+
         //Знайти кількість потенційно працездатних людей у вибірці (тобто від 18 років і з огляду на що жінки виходять в 55 років, а чоловік в 60)
         peoples.stream()
                 .filter(people -> people.getAge() >= 18)
                 .filter(people -> (people.getSex().equalsIgnoreCase("male") && people.getAge() < 60) || (people.getSex().equalsIgnoreCase("female") && people.getAge() < 55))
                 .count();
+
         //Відсортувати колекцію людей за ім'ям в зворотному алфавітному порядку
         peoples.stream()
                 .sorted((o1, o2) -> o2.getName().compareTo(o1.getName()))
                 .collect(Collectors.toList());
+
         //Відсортувати колекцію людей спочатку за ім’ям, а потім за віком
         peoples.stream()
                 .sorted((o1, o2) -> o1.getName() != o2.getName() ? o1.getName().compareTo(o2.getName()): o1.getAge().compareTo(o2.getAge()))
                 .collect(Collectors.toList());
+
         //Знайти найстаршу людину
         peoples.stream().max((o1, o2) -> o1.getAge().compareTo(o2.getAge())).get();
+
         //Знайти наймолодшу людину
         peoples.stream().min((o1, o2) -> o1.getAge().compareTo(o2.getAge())).get();
+
         //Вивести скільки є чоловіків
         peoples.stream()
                 .filter(people -> people.getSex().equalsIgnoreCase("male"))
                 .count();
+
         //Вивести скільки є жінок
         peoples.stream()
                 .filter(people -> people.getSex().equalsIgnoreCase("female"))
                 .count();
+
         //Вивести всіх жінок в яких ім’я починається на “A”
         peoples.stream()
                 .filter(people -> people.getSex().equalsIgnoreCase("female") && people.getName().startsWith("S"))
